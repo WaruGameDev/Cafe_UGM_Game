1d499cc448ce40e4cefcab41a2e228299b5d76eb
WaruGameDev <60048774+WaruGameDev@users.noreply.github.com>
2025-08-11 11:50:32 -0400

    ejemplo cliente

---
+++ Assets/Scripts/Cliente/Client.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum ClientStates
+{
+    WAITING,
+    GOING_TO_TABLE,
+    THINKING,
+    ORDERNING,
+    EATING,
+    LEAVING 
+}
+
+[System.Serializable]
+public class ClientClass
+{
+    public string nameClient;
+    public ClientStates clientState = ClientStates.WAITING;
+
+} 
+
+public class Client : MonoBehaviour
+{
+    public ClientClass client;
+    private NavMeshAgent navMeshAgent;
+    public Transform target;
+   
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (client.clientState)
+        {
+            case ClientStates.GOING_TO_TABLE:
+                navMeshAgent.SetDestination(target.position);
+                break;
+        }
+    }
+}
